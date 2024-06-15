d039d56df7e789f5ddfe6e18f605d283e1381fe6
MoMansur <mansurshehumsm@gmail.com>
2024-06-15 19:56:49 +0300

    DOM element invoked

---
+++ script.js
@@ -0,0 +1,13 @@
+const DOMgrid = document.querySelectorAll('.cell')
+const gameBoxDom = document.querySelector('.gameBox')
+const statusMessage = document.getElementById('statusMessage')
+
+//SCORES
+const playerOneNameDom = document.querySelector('.name1')
+const playerOneScoreDom = document.querySelector('.score1')
+
+const playerTwoNameDom = document.querySelector('.name2')
+const playerTwoScoreDom = document.querySelector('.score2')
+
+//Reset Buttons
+const resetScores = document.getElementById('resetScores')
